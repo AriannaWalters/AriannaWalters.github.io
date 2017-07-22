50146d2956250dfe914a6bd7bf81834f25536f77
Arianna Walters <ari1471@hotmail.com>
2017-07-22 12:59:26 -0400

    Add files via upload

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,11 +1,11 @@
 //alert ("Welcome!") ;
 console.log ("This website belongs to Arianna Walters.") ;
 document.write ("Arianna Walters' Page") ;
-function IDK() {
-  var weeks = 4;
-  var user = "Arianna";
+//function IDK() {
+//  var weeks = 4;
+//  var user = "Arianna";
 
-  document.write(" " + user + " has been coding for " + weeks + " weeks.");
+//  document.write(" " + user + " has been coding for " + weeks + " weeks.");
 
-}
-IDK();
+//}
+//IDK();
